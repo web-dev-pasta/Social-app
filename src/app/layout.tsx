a0bbcead14eac9b10ad10dd323c3b2f357f2643c
web-dev-pasta <on5100832@gmail.com>
2026-03-26 23:26:21 +0200

    Added Light, Dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,14 +24,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.className} `}>
+    <html
+      lang="en"
+      className={`${geistSans.className}`}
+      suppressHydrationWarning
+    >
       <body className="flex min-h-full flex-col">
-        <TooltipProvider>
-          <main>
-            {children}
-            <Toaster />
-          </main>
-        </TooltipProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TooltipProvider>
+            <main>
+              {children}
+              <Toaster />
+            </main>
+          </TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
